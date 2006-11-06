62420d0e4de65387f1c5e974e16fe20601f34140
kohsuke <kohsuke@dev.java.net>
2006-11-06 13:39:28 +0000

    d'oh!

--- src/main/java/hudson/plugins/promoted_builds/PluginImpl.java
+++ src/main/java/hudson/plugins/promoted_builds/PluginImpl.java
@@ -1,12 +1,13 @@
 package hudson.plugins.promoted_builds;
 
 import hudson.model.Jobs;
+import hudson.Plugin;
 
 /**
  * @author Kohsuke Kawaguchi
  * @plugin
  */
-public class PluginImpl {
+public class PluginImpl extends Plugin {
     public void start() throws Exception {
         Jobs.JOBS.add(PromotedJob.DESCRIPTOR);
     }
