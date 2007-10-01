fb46625b735e0a996efe476da157789450d53f53
kohsuke <kohsuke@dev.java.net>
2007-10-01 22:22:27 +0000

    action to be added to builds.

---
+++ src/main/java/hudson/plugins/promoted_builds/PromotedBuildAction.java
@@ -0,0 +1,50 @@
+package hudson.plugins.promoted_builds;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+
+/**
+ * {@link Action} for {@link AbstractBuild} indicating that it's promoted.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public final class PromotedBuildAction implements Action {
+    /**
+     * List of promotion criteria names that this build qualified.
+     */
+    private volatile String[] promotions;
+
+    public PromotedBuildAction(PromotionCriterion criterion) {
+        promotions = new String[]{criterion.getName()};
+    }
+
+    /**
+     * Called when the build passes another promotion criterion.
+     */
+    public synchronized void add(PromotionCriterion criterion) {
+        String n = criterion.getName();
+        for (String p : promotions)
+            if(n.equals(p))
+                return; // noop
+
+        String[] r = new String[promotions.length+1];
+        System.arraycopy(promotions,0,r,0,promotions.length);
+        r[promotions.length] = n;
+
+        // atomically replace it
+        this.promotions = r;
+    }
+
+    public String getIconFileName() {
+        // TODO: fix this
+        return "health-80plus.gif";
+    }
+
+    public String getDisplayName() {
+        return "Promotion Status";
+    }
+
+    public String getUrlName() {
+        return "promoted";
+    }
+}
