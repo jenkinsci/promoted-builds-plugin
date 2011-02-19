9ad9ab8ac3745e66e77235d0164324c5efc9abe1
petehayes <petehayes@gmail.com>
2011-02-19 14:23:39 -0500

    Added build id of the promoted build to the promotion environment

--- src/main/java/hudson/plugins/promoted_builds/Promotion.java
+++ src/main/java/hudson/plugins/promoted_builds/Promotion.java
@@ -78,6 +78,7 @@ public class Promotion extends AbstractBuild<PromotionProcess,Promotion> {
                 e.put("PROMOTED_URL",rootUrl+getTarget().getUrl());
             e.put("PROMOTED_JOB_NAME", getTarget().getParent().getName());
             e.put("PROMOTED_NUMBER", Integer.toString(targetBuildNumber));
+            e.put("PROMOTED_ID", getTarget().getId());
         }
         return e;
     }
