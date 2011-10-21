812246dbb44c93d8c08db81524344357fbb5a0fe
Grant Limberg <glimberg@gmail.com>
2011-10-21 15:11:18 -0700

    Added a parameterized self promotion process.
    If a given build parameter matches the given value, the build is self-promoted.  Otherwise, the promotion is not run.  This allows the user to automatically run different promotion processes based on build paramters.

---
+++ src/main/java/hudson/plugins/promoted_builds/conditions/ParameterizedSelfPromotionBadge.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hudson.plugins.promoted_builds.conditions;
+
+import hudson.plugins.promoted_builds.PromotionBadge;
+
+/**
+ *
+ * @author grant
+ */
+public class ParameterizedSelfPromotionBadge extends PromotionBadge {
+
+}
