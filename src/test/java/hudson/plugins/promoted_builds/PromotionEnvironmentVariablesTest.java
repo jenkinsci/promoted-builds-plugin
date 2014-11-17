40243b5912ec8b863e5ad938e926ebea00ce0e48
Jesse Glick <jglick@cloudbees.com>
2014-11-17 18:13:33 -0500

    Simplified and fixed test so that it still passes against 1.580.1.

--- src/test/java/hudson/plugins/promoted_builds/PromotionEnvironmentVariablesTest.java
+++ src/test/java/hudson/plugins/promoted_builds/PromotionEnvironmentVariablesTest.java
@@ -1,32 +1,21 @@
 package hudson.plugins.promoted_builds;
 
 import hudson.EnvVars;
-import hudson.Extension;
-import hudson.model.AbstractItem;
 import hudson.model.Action;
 import hudson.model.Descriptor;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
-import hudson.model.ItemGroup;
-import hudson.model.Job;
 import hudson.model.ParameterValue;
 import hudson.model.TaskListener;
-import hudson.model.TopLevelItem;
-import hudson.model.TopLevelItemDescriptor;
 import hudson.plugins.promoted_builds.conditions.ManualCondition;
-import hudson.util.DescribableList;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import jenkins.model.Jenkins;
 import org.junit.Test;
 import org.junit.Rule;
 import org.jvnet.hudson.test.JenkinsRule;
 import static org.junit.Assert.*;
+import org.jvnet.hudson.test.MockFolder;
 
 /**
  * @author Jonathan Zimmerman
@@ -39,13 +28,11 @@ public class PromotionEnvironmentVariablesTest {
     @Test
     public void shouldSetJobAndJobFullNames() throws Descriptor.FormException, IOException, InterruptedException, ExecutionException {
         // Assemble
-        MockFolder parent = new MockFolder(r.jenkins, "Folder");
-        parent.save();
-        FreeStyleProject project = new FreeStyleProject(parent, "Project");
-        project.save();
+        MockFolder parent = r.createFolder("Folder");
+        FreeStyleProject project = parent.createProject(FreeStyleProject.class, "Project");
         
         JobPropertyImpl promotionProperty = new JobPropertyImpl(project);
-        PromotionProcess promotionProcess = new PromotionProcess(promotionProperty, "promo");
+        PromotionProcess promotionProcess = promotionProperty.addProcess("promo");
         promotionProcess.conditions.clear();
         promotionProcess.conditions.add(new ManualCondition());
         Action approvalAction = new ManualCondition.ManualApproval(promotionProcess.getName(), new ArrayList<ParameterValue>());
@@ -71,75 +58,4 @@ public class PromotionEnvironmentVariablesTest {
         parent.delete();
     }
     
-    private static class MockFolder extends AbstractItem implements TopLevelItem, ItemGroup<TopLevelItem> {
-
-        private DescribableList<TopLevelItem, TopLevelItemDescriptor> items;
-        
-        public MockFolder(ItemGroup parent, String name) {
-            super(parent, name);
-            
-            if (items == null) {
-                items = new DescribableList(this);
-            }
-        }
-
-        public TopLevelItemDescriptor getDescriptor() {
-            return Jenkins.getInstance().getDescriptorByType(DescriptorImpl.class);
-        }
-
-        public Collection<TopLevelItem> getItems() {
-            return items.toList();
-        }
-
-        public String getUrlChildPrefix() {
-            return "job";
-        }
-
-        public TopLevelItem getItem(String name) {
-            for (TopLevelItem item : getItems()) {
-                if (item.getName().equals(name)) {
-                    return item;
-                }
-            }
-            
-            return null;
-        }
-
-        public File getRootDirFor(TopLevelItem child) {
-            return new File(getParent().getRootDir(), name + "/jobs/" + child.getName());
-        }
-
-        public void onRenamed(TopLevelItem item, String oldName, String newName) throws IOException {
-        }
-
-        public void onDeleted(TopLevelItem item) throws IOException {
-        }
-     
-        @Override
-        public Collection<? extends Job> getAllJobs() {
-            List<Job> jobs = Collections.emptyList();
-            
-            for (TopLevelItem item : getItems()) {
-                if (item instanceof Job) {
-                    jobs.add((Job)item);
-                }
-            }
-            
-            return jobs;
-        }
-        
-        @Extension
-        public static final class DescriptorImpl extends TopLevelItemDescriptor {
-
-            @Override
-            public String getDisplayName() {
-                return "";
-            }
-
-            @Override
-            public TopLevelItem newInstance(ItemGroup parent, String name) {
-                return new MockFolder(parent, name);
-            }
-        }
-    }
 }
