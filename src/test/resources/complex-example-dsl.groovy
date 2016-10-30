49d8fdbd78d747791e8e828934411ac2adf9d0aa
Fiouz <fiouzy@gmail.com>
2016-10-30 23:26:48 +0800

    Increase test coverage for static DSL

--- src/test/resources/complex-example-dsl.groovy
+++ src/test/resources/complex-example-dsl.groovy
@@ -1,16 +1,16 @@
 freeStyleJob('test-job-complex') {
-    properties{
-      promotions {
+    properties {
+        promotions {
             promotion {
                 name("Development")
                 icon("star-red")
                 restrict('slave1')
                 conditions {
-                    manual('testuser'){
-                      parameters{
-                          textParam("parameterName","defaultValue","description")
-                      }
-                     }
+                    manual('testuser') {
+                        parameters {
+                            textParam("parameterName", "defaultValue", "description")
+                        }
+                    }
                 }
                 actions {
                     downstreamParameterized {
@@ -21,9 +21,9 @@ freeStyleJob('test-job-complex') {
                                 unstable('UNSTABLE')
                             }
                             parameters {
-                                predefinedProp("ENVIRONMENT","dev-server")
+                                predefinedProp("ENVIRONMENT", "dev-server")
                                 predefinedProp("APPLICATION_NAME", "\${PROMOTED_JOB_FULL_NAME}")
-                                predefinedProp("BUILD_ID","\${PROMOTED_NUMBER}")
+                                predefinedProp("BUILD_ID", "\${PROMOTED_NUMBER}")
                             }
                         }
                     }
@@ -46,12 +46,51 @@ freeStyleJob('test-job-complex') {
                                 unstable('UNSTABLE')
                             }
                             parameters {
-                                predefinedProp("ENVIRONMENT","test-server")
+                                predefinedProp("ENVIRONMENT", "test-server")
                                 predefinedProp("APPLICATION_NAME", "\${PROMOTED_JOB_FULL_NAME}")
-                                predefinedProp("BUILD_ID","\${PROMOTED_NUMBER}")
+                                predefinedProp("BUILD_ID", "\${PROMOTED_NUMBER}")
                             }
                         }
                     }
+                }
+            }
+            promotion {
+                name('selfPromotion')
+                conditions {
+                    selfPromotion(false)
+                }
+            }
+            promotion {
+                name('parameterizedSelfPromotion')
+                conditions {
+                    parameterizedSelfPromotion(false, 'PARAM', 'value')
+                }
+            }
+            promotion {
+                name('releaseBuild')
+                conditions {
+                    releaseBuild()
+                }
+            }
+            promotion {
+                name('downstream')
+                conditions {
+                    downstream(false, 'jobName')
+                }
+            }
+            promotion {
+                name('upstream')
+                conditions {
+                    upstream('jobName')
+                }
+            }
+            promotion {
+                name('parameters deprecation')
+                conditions {
+                    // This is not expected to achieve anything but was wrongly exposed at that level, we just test it
+                    // to make sure we can properly keep compatibility while also displaying a deprecation message to
+                    // the user
+                    parameters {}
                 }
             }
         }
