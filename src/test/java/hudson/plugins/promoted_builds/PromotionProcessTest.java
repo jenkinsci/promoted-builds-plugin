3f87d4ece5b7f644cc59162cb269f7f00170117a
Baptiste Mathus <batmat@batmat.net>
2022-02-08 13:06:01 +0100

    Switch to using curl instead of powershell
    Should be standard on most Windows OSes nowadays (thanks @jnord).

--- src/test/java/hudson/plugins/promoted_builds/PromotionProcessTest.java
+++ src/test/java/hudson/plugins/promoted_builds/PromotionProcessTest.java
@@ -63,9 +63,7 @@ public class PromotionProcessTest {
         String baseUrl = j.createWebClient().getContextPath() + "job/up/lastSuccessfulBuild";
         String artifactUrl = baseUrl + "/artifact/a.jar";
         down.getBuildersList().add(Functions.isWindows()
-                ? new BatchFile("powershell -command \"Invoke-WebRequest "+artifactUrl+" -OutFile a.jar\"\r\n"+
-                        "set /a \"exitCode=BUILD_NUMBER%%2\"\r\n"+
-                        "exit /b %exitCode%\r\n")
+                ? new BatchFile("curl --output a.out "+ artifactUrl)
                 : new Shell("wget -N "+artifactUrl+" \\\n"+
                         "  || curl "+artifactUrl+" > a.jar\n"+
                         "expr $BUILD_NUMBER % 2 - 1\n") // expr exits with non-zero status if result is zero
