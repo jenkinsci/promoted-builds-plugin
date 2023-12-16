dc3a74929864a36ef38baf654b634ffaf4787b13
Mark Waite <mark.earl.waite@gmail.com>
2023-12-16 16:46:14 -0700

    Replace deprecated assertThat with hamcrest assertThat

--- src/test/java/hudson/plugins/promoted_builds/conditions/ManualConditionTest.java
+++ src/test/java/hudson/plugins/promoted_builds/conditions/ManualConditionTest.java
@@ -41,11 +41,17 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.JenkinsRule.WebClient;
 import org.jvnet.hudson.test.MockAuthorizationStrategy;
 
-import static org.htmlunit.html.HtmlFormUtil.submit;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.htmlunit.html.HtmlFormUtil.submit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * @author Kohsuke Kawaguchi
